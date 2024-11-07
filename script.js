65a0706f0ffa4f4642f5335df4322fcfac640d87
boukadia <mustapha.boukadia97@gmail.com>
2024-11-08 00:37:05 +0100

    troisieme-commit

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 let next = document.getElementById("next");
-let sec0 = document.getElementById("sec0");
-let sec1 = document.getElementById("sec1");
-let sec2 = document.getElementById("sec2");
-let sec3 = document.getElementById("sec3");
-let sec4 = document.getElementById("sec4");
-var i = 0;
-// console.log(typeof 'sec0')
-var secList = [];
-const list = {
-  sec0: sec0,
-  sec1: sec1,
-  sec2: sec2,
-  sec3: sec3,
-  sec4: sec4,
-};
-secList.push(list);
-console.log(secList);
-// const array=["sec0","sec1","sec2","sec3","sec4"]
+let sec0 = document.getElementById("tab-0");
+let sec1 = document.getElementById("tab-1");
+let sec2 = document.getElementById("tab-2");
+let sec3 = document.getElementById("tab-3");
+let sec4 = document.getElementById("tab-4");
+var i=0;
+var tabList=[sec1,sec2,sec3,sec4];
 
-console.log(next);
-next.onclick = function (i) {
-  sec1.style.display = "flex";
+function nextSection(){
+  i++;
+  // if(i>4)
+  //   i=0;
+  
 
-  secList[1].scrollIntoView({ behavior: "smooth", display: "block" });
-};
+  tabList[i].style.display = "flex";
+
+  tabList[i].scrollIntoView({ behavior: "smooth", display: "block" });
+ 
+  
+
+}
+
